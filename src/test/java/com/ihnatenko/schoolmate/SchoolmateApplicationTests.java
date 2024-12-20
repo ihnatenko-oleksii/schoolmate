5d87fb77670d2c9c831b0e2fccf6d4ba10fe1d53
ihnatenko-oleksii <ignatenkolesha123@gmail.com>
2024-12-20 11:11:43 +0100

    Initialization project

---
+++ src/test/java/com/ihnatenko/schoolmate/SchoolmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihnatenko.schoolmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
